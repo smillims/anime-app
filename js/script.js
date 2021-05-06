411313fe8e7c73898894f9ebc089e318102e6776
smillims <mrdenkil777@gmail.com>
2021-05-06 20:59:06 +0300

    v1.1 - server version

--- js/script.js
+++ js/script.js
@@ -1,25 +1,106 @@
-import { results } from './api-json';
 import styles from '../css/style.css';
 
-window.results = results;
 window.renderApp = renderApp;
-window.filterSearch = filterSearch;
-window.cardsToShow = results;
+window.performSearch = performSearch;
+window.validateAndLoadData = validateAndLoadData;
 
 window.dataStore = {
   currentTitle: '',
+  isDataLoading: false,
+  error: null,
+  cashOfAnimeSearch: {},
 };
+
+const animeTitles = ['One Piece', 'Tokyo Ghoul'];
+const allowedTitles = Object.values(animeTitles);
+
+function getAnimeSearchUrl(search) {
+  const checkTitle = allowedTitles.filter(animeTitle => animeTitle === search);
+  const stringToUrl = checkTitle.join().replace(/\s+/g, '');
+  return `https://api.jikan.moe/v3/search/anime?q=${stringToUrl}&page=1`;
+}
+
+function isCurrentTitleDataLoaded() {
+  const { cashOfAnimeSearch, currentTitle } = window.dataStore;
+  return cashOfAnimeSearch[currentTitle];
+}
+
+function validateAndLoadData() {
+  const { currentTitle } = window.dataStore;
+
+  if (!allowedTitles.includes(currentTitle)) {
+    const error = `Enter one of the anime titles: ${allowedTitles.join(', ')}.`;
+    return Promise.resolve({ error });
+  }
+
+  const url = getAnimeSearchUrl(currentTitle);
+  if (!isCurrentTitleDataLoaded()) {
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        return { data };
+      });
+  }
+
+  return Promise.resolve({});
+}
+
+function performSearch(animeTitle) {
+  window.dataStore.currentTitle = animeTitle;
+  window.dataStore.error = null;
+  window.dataStore.isDataLoading = true;
+
+  window
+    .validateAndLoadData()
+    .then(({ error, data }) => {
+      window.dataStore.isDataLoading = false;
+      if (error) {
+        window.dataStore.error = error;
+      } else if (data) {
+        window.dataStore.cashOfAnimeSearch[animeTitle] = data.results;
+      }
+    })
+    .catch(() => {
+      window.dataStore.error = 'Happened some error.';
+    })
+    .finally(() => {
+      window.renderApp();
+    });
+}
+
+renderApp();
 
 function renderApp() {
   document.querySelector('#app').innerHTML = App();
 }
 
-renderApp();
+function AnimeCardsResults() {
+  const { currentTitle, isDataLoading, error } = window.dataStore;
+
+  let content = '';
+  if (currentTitle === '') {
+    content = 'Search anime whatever you like';
+  } else {
+    if (isDataLoading) {
+      content = 'Loading...';
+    }
+
+    if (error !== null) {
+      content = error;
+    }
+
+    if (isCurrentTitleDataLoaded()) {
+      content = `${CardsList(isCurrentTitleDataLoaded())}`;
+    }
+  }
+
+  return `<p class='${styles.paragraphOfState}'>${content}</p>`;
+}
 
 function App() {
   return `${Header()}
           ${SortForm()}
-          ${CardsList()}`;
+          ${AnimeCardsResults()}`;
 }
 function Header() {
   return `<header class="${styles.header}">
@@ -28,9 +109,7 @@ function Header() {
 }
 function SortForm() {
   return ` <div class="${styles.sectionForm}">
-              <form class="${
-                styles.sortForm
-              }" id="sortForm" name="sortForm" onsubmit="window.filterSearch(); window.renderApp()">
+              <form class="${styles.sortForm}" id="sortForm" name="sortForm">
                 <div class="${styles.blockSearch}">
                   ${SearchInputSortForm()}
                   ${ApplyButtonSortForm()}
@@ -45,7 +124,7 @@ function SearchInputSortForm() {
                 type="text"
                 name="search"
                 value="${window.dataStore.currentTitle}"
-                onchange="window.dataStore.currentTitle = this.value;"
+                onchange="window.performSearch(this.value)"
                 autofocus
           />`;
 }
@@ -53,32 +132,25 @@ function ApplyButtonSortForm() {
   return `<button class="${styles.formButton}" 
                   id="formButton" 
                   type="button" 
-                  onclick="window.filterSearch(); window.renderApp()">
+                  name="searchButton">
                     Button
             </button>`;
 }
-function CardsList() {
+function CardsList(animeSearch) {
+  const form = document.forms.sortForm;
+  const search = form.search.value;
   return `<main class="${styles.main}">
             <div class="${styles.cardsItems}" id="cardsItems">
-              ${cardsToShow
+              ${animeSearch
                 .filter(anime => {
                   if (anime.rated === 'Rx') return false;
+                  else if (!anime.title.includes(search)) return false;
                   return true;
                 })
                 .map(card => Card(card))
                 .join('')}
             </div>
           </main>`;
-}
-
-function filterSearch() {
-  const form = document.forms.sortForm;
-  const search = form.search.value;
-  if (!search) return (window.cardsToShow = results);
-  window.cardsToShow = results.filter(animeItem => {
-    if (!(`${animeItem.title}`.toLowerCase().includes(search.toLowerCase()) && search)) return false;
-    return true;
-  });
 }
 
 function Card(card) {
